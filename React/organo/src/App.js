b141427e55c4c8d4f3bf8366886bf17db0592778
IgorSandes <igorsandes213@gmail.com>
2023-12-17 16:53:45 -0300

    Adicionando projeto React

---
+++ React/organo/src/App.js
@@ -0,0 +1,13 @@
+import Banner from './componentes/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="banner">
+        <Banner />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <img src="/imagens/imagens/banner.png" alt="O banner principal da página do Organo" />
+    )
+}
+
+export default Banner
